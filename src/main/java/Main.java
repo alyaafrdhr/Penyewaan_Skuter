b8f875823b53010be65b77f9200a91e3f4c4bd77
alyaafrdhr <aliya.iphone02@gmail.com>
2022-07-03 18:08:07 +0700

    PemesananViewFrame dan PemesananInputFrame meniru dari Peminjam

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,13 @@
-import frame.PeminjamViewFrame;
+//import frame.PeminjamViewFrame;
+import frame.PemesananViewFrame;
 import helpers.Koneksi;
 
 public class Main {
     public static void main(String[] args) {
         Koneksi.getConnection();
-        PeminjamViewFrame viewFrame = new PeminjamViewFrame();
+//        PeminjamViewFrame viewFrame = new PeminjamViewFrame();
+//       viewFrame.setVisible(true);
+        PemesananViewFrame viewFrame = new PemesananViewFrame();
         viewFrame.setVisible(true);
     }
 }
---
+++ src/main/java/frame/PemesananViewFrame.java
@@ -0,0 +1,174 @@
+package frame;
+
+import helpers.Koneksi;
+import org.bouncycastle.jcajce.provider.drbg.DRBG;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.sql.*;
+public class PemesananViewFrame extends JFrame {
+    private JPanel mainPanel;
+    private JPanel cariPanel;
+    private JTextField cariTextField;
+    private JButton cariButton;
+    private JScrollPane viewScrollPane;
+    private JTable viewTable;
+    private JPanel buttonPanel;
+    private JButton tambahButton;
+    private JButton ubahButton;
+    private JButton hapusButton;
+    private JButton batalButton;
+    private JButton cetakButton;
+    private JButton tutupButton;
+
+    public PemesananViewFrame(){
+        ubahButton.addActionListener(e ->{
+            int barisTerpilih = viewTable.getSelectedRow();
+            if (barisTerpilih < 0) {
+                JOptionPane.showMessageDialog(null,
+                        "Pilih data dulu",
+                        "Validasi pilih data",
+                        JOptionPane.WARNING_MESSAGE);
+                return;
+            }
+
+            TableModel tm = viewTable.getModel();
+            int id = Integer.parseInt(tm.getValueAt(barisTerpilih, 0).toString());
+            PemesananInputFrame inputFrame = new PemesananInputFrame();
+            inputFrame.setId(id);
+            inputFrame.isiKomponen();
+            inputFrame.setVisible(true);
+        });
+
+        tambahButton.addActionListener(e ->{
+            PemesananInputFrame inputFrame = new PemesananInputFrame();
+            inputFrame.setVisible(true);
+        });
+
+        hapusButton.addActionListener(e -> {
+            int barisTerpilih = viewTable.getSelectedRow();
+            if (barisTerpilih < 0) {
+                JOptionPane.showMessageDialog(null,
+                        "Pilih data dulu",
+                        "Validasi pilih data",
+                        JOptionPane.WARNING_MESSAGE);
+                return;
+            }
+            int pilihan = JOptionPane.showConfirmDialog(null,
+                    "Yakin mau hapus?",
+                    "Konfirmasi Hapus",
+                    JOptionPane.YES_NO_OPTION);
+
+            if(pilihan == 0) { //pilihan YES
+                TableModel tm = viewTable.getModel();
+                int id = Integer.parseInt(tm.getValueAt(barisTerpilih, 0).toString());
+
+                Connection c = Koneksi.getConnection();
+                String deleteSQL = "DELETE FROM pemesanan WHERE id = ?";
+                try {
+                    PreparedStatement ps = c.prepareStatement(deleteSQL);
+                    ps.setInt(1, id);
+                    ps.executeUpdate();
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+
+        cariButton.addActionListener(e -> {
+            if(cariTextField.getText().equals("")){
+                JOptionPane.showMessageDialog(null,
+                        "Isi kata kunci pencarian",
+                        "Validasi kata kunci kosong",
+                        JOptionPane.WARNING_MESSAGE);
+                cariTextField.requestFocus();
+                return;
+            }
+            String keyword= "%" + cariTextField.getText() + "%";
+            String searchSQL = "SELECT * FROM pemesanan WHERE nama like ?";
+            Connection c = Koneksi.getConnection();
+
+            try {
+                PreparedStatement ps = c.prepareStatement(searchSQL);
+                ps.setString(1, keyword);
+                ResultSet rs = ps.executeQuery();
+
+                String[] header = {"Id","Nama Skuter"};
+                DefaultTableModel dtm = new DefaultTableModel(header,0);
+                viewTable.setModel(dtm);
+                viewTable.getColumnModel().getColumn(0).setWidth(100);
+                viewTable.getColumnModel().getColumn(0).setMaxWidth(100);
+                viewTable.getColumnModel().getColumn(0).setMinWidth(100);
+                viewTable.getColumnModel().getColumn(0).setPreferredWidth(100);
+
+                Object[] row = new Object[2];
+                while (rs.next()){
+                    row[0] = rs.getInt("id");
+                    row[1] = rs.getString("nama");
+                    dtm.addRow(row);
+                }
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        tutupButton.addActionListener(e -> {
+            dispose();
+        });
+        batalButton.addActionListener(e -> {
+            isiTable();
+        });
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowActivated(WindowEvent e) {
+                isiTable();
+            }
+
+        });
+        isiTable();
+        init();
+    }
+
+    public void init() {
+        setContentPane(mainPanel);
+        setTitle("Data Pemesanan");
+        pack();
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+    }
+
+    public void isiTable(){
+        String selectSQL = "SELECT * FROM pemesanan";
+        Connection c = Koneksi.getConnection();
+
+        try {
+            Statement s = c.createStatement();
+            ResultSet rs = s.executeQuery(selectSQL);
+
+            String[] header = {"Id","Nama Skuter"};
+            DefaultTableModel dtm = new DefaultTableModel(header,0);
+            viewTable.setModel(dtm);
+
+            viewTable.getColumnModel().getColumn(0).setWidth(100);
+            viewTable.getColumnModel().getColumn(0).setMaxWidth(100);
+            viewTable.getColumnModel().getColumn(0).setMinWidth(100);
+            viewTable.getColumnModel().getColumn(0).setPreferredWidth(100);
+
+            Object[] row = new Object[2];
+            while (rs.next()){
+                row[0] = rs.getInt("id");
+                row[1] = rs.getString("nama");
+                dtm.addRow(row);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
+
+
